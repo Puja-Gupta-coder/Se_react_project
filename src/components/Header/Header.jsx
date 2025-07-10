2d172c49dff07f578db88ed5395f4a0b835f9fbf
Puja Gupta <Puja.nathani123@gmail.com>
2025-07-09 23:39:07 -0500

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import "./Header.css";
+import logo from "../../assets/logo.png";
+import avatar from "../../assets/avatar.png";
 
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" />
+      <img className="header__logo" src={logo} />
       <p className="header__date-and-location">DATE,LOCATION</p>
       <button className="header__add-clothes-btn">Add clothes</button>
       <div className="header__user-container">
-        <p className="header__username">Name</p>
-        <img src="" alt="" className="header__avatar" />
+        <p className="header__username">Terrence Tegenge</p>
+        <img src={avatar} alt="Terrence Tegenge" className="header__avatar" />
       </div>
     </header>
   );
